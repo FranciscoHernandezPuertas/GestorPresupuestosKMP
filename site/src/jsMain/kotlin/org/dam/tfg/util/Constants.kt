0f0ee23677c84b477c08e41500b0e9be9d7cc0f5
FranciscoHernandezPuertas <franhernandezpuertas@outlook.com>
2025-01-25 15:01:36 +0100

    Inicial: Configuración base del proyecto Kobweb con Gradle y Kotlin
    - Agregado archivo .gitignore para ignorar archivos y carpetas de sistema, IDE (IntelliJ) y Gradle.
    - Configurado el build.gradle.kts raíz con plugins y repositorios necesarios.
    - Añadidos gradle.properties con ajustes de estilo y caching.
    - Definidas las versiones de dependencias y plugins en gradle/libs.versions.toml.
    - Configurado el Gradle Wrapper mediante gradle/wrapper/gradle-wrapper.properties.
    - Configurada la estructura del módulo web:
        • site/.gitignore y site/.kobweb/conf.yaml con parámetros básicos (título, configuración del servidor y rutas).
        • site/build.gradle.kts para la aplicación Kobweb y sus dependencias.
        • Creados los puntos de entrada de la aplicación en site/src/jsMain/kotlin/org/dam/tfg/AppEntry.kt y la página principal en Index.kt.

---
+++ site/src/jsMain/kotlin/org/dam/tfg/util/Constants.kt
@@ -0,0 +1,11 @@
+package org.dam.tfg.util
+
+object Constants {
+    const val FONT_FAMILY = "Roboto"
+}
+
+object Res {
+    object Image{
+        const val logo = "/Generador de Presupuestos KMP Logo.svg"
+    }
+}
